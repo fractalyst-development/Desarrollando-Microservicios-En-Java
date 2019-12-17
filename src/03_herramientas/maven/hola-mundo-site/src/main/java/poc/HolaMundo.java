6bb6536f295d9f9eb05bc5f6b56998a4e563e7f2
Pedro Cazu <training@pcabrera.com>
2019-12-16 23:15:52 -0600

    more fixes

---
+++ src/03_herramientas/maven/hola-mundo-site/src/main/java/poc/HolaMundo.java
@@ -0,0 +1,13 @@
+
+package poc;
+/**
+* Hola Mundo
+*/
+public class HolaMundo {
+/**
+ * Método main
+ */
+public static void main (String args []) {
+System.out.println ("Hola mundo, Maven");
+}
+}
